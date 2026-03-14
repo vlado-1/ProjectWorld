992222b2f755a32936d38c039799e39c92d7e2fe
vlado-1 <vladosmolovic18@yahoo.com>
2026-03-14 14:35:05 +1100

    Make info in popups concise and clear.

--- scripts/setup-data.ts
+++ scripts/setup-data.ts
@@ -14,7 +14,7 @@ initSqlJs({ locateFile: (file: string) => dbConfig.assemblyFile }).then((SQL) =>
   const CreateDataQuery: string = `INSERT INTO projects VALUES 
       ( 5, 5, 'Unity Development', 'Completed a unity junior developer course, and developed some beginner level games using the Unity Game Engine.', 'assets/images/Unity_Country.png', 'https://www.youtube.com/embed/06FxP4wr36I?si=-2jV8zHvfe_i8SFv' ),
       ( 30, 60, 'Bravura Solutions', 'Work on maintaining and enhancing a desktop application for funds administration.', 'assets/images/Bravura_Country.png', '' ),
-      ( -30, 120, 'Website Development', 'Developed this dynamic website using Angular and SQLite.', 'assets/images/Angular_Country.png', 'https://www.youtube.com/embed/TuaDNw67W_0?si=W_2BKLN6S7d2_7t1' ),
+      ( -30, 120, 'Website Development', 'Developed two websites using Angular. This one, and another shown below that I have not hosted.', 'assets/images/Angular_Country.png', 'https://www.youtube.com/embed/TuaDNw67W_0?si=W_2BKLN6S7d2_7t1' ),
       ( 10, 200, 'University', 'Completed a bachelor and masters degree majoring in computer science and software development respectively.', 'assets/images/Usyd_Country.png', '' );
 `;
   db.exec(CreateTableQuery);
